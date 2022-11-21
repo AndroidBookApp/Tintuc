9d5e9e28ea521449c8b3fdd5c2de4c5af12fd266
kiemluc01 <kiemluc01@gmail.com>
2022-11-21 10:09:00 +0700

    vendor

---
+++ resources/views/layout/main-layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>demo</title>
+
+    <link rel="stylesheet" href="{{ asset('css/demo.css') }}">
+</head>
+<body>
+    <div class="container">
+        <table>
+            <tr>
+                <th>1</th>
+                <th>23</th>
+                <th>3</th>
+                <th>4</th>
+                <th>5</th>
+                <th>6</th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
