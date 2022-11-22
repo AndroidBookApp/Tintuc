1e3b76265efd213fa32729d2d1afcb2559a75f6c
kiemluc01 <kiemluc01@gmail.com>
2022-11-22 13:40:33 +0700

    img, css

--- resources/views/layout/main-layout.blade.php
+++ resources/views/layout/main-layout.blade.php
@@ -5,21 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>demo</title>
-
-    <link rel="stylesheet" href="{{ asset('css/demo.css') }}">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
 <body>
-    <div class="container">
-        <table>
-            <tr>
-                <th>1</th>
-                <th>23</th>
-                <th>3</th>
-                <th>4</th>
-                <th>5</th>
-                <th>6</th>
-            </tr>
-        </table>
+    <div class="header">
+        @include('.static-layout.header')
     </div>
+    <div class="content-container">
+       @yield('content')
+    </div>
+    <div class="footer">
+        @include('.static-layout.footer')
+    </div>
+    <!-- script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="assets/js/live-search.js"></script>
 </body>
 </html>
