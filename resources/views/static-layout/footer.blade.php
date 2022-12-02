b307350068a2b1ac3ab8092d29c2421ad38a64a4
kiemluc01 <kiemluc01@gmail.com>
2022-12-02 19:00:54 +0700

    UI footer

--- resources/views/static-layout/footer.blade.php
+++ resources/views/static-layout/footer.blade.php
@@ -1,16 +1,61 @@
 <div class="footer">
-<hr>
-    <div class="ft-top">
-        <div class="ftt-left">
-            <p >Tin tức UTE</p>
-            <img src="assets/image/icon/logo-long.png" alt="logo-long" class="logo-ft">
-        </div>
-        <div class="ftt-right">
-            <p>Thông tin thêm tại: </p>
-            <div>
-                
+    <hr>
+    <div class="footer-menu">
+        <ul class="menu-main">
+            <li><a href="">Trang chủ</a></li>
+            <li><a href="">Video</a></li>
+            <li><a href="">Podcasts</a></li>
+            <li><a href="">Ảnh</a></li>
+            <li><a href="">inforGraphics</a></li>
+        </ul>
+        <ul class="menu-submain">
+            <li><a href="">Thời sự</a></li>
+            <li><a href="">góc nhìn</a></li>
+            <li><a href="">thế giới</a></li>
+            <li><a href="">kinh doanh</a></li>
+            <li><a href="">giải trí</a></li>
+        </ul>
+        <ul class="menu-submain">
+            <li><a href="">Thời sự</a></li>
+            <li><a href="">góc nhìn</a></li>
+            <li><a href="">thế giới</a></li>
+            <li><a href="">kinh doanh</a></li>
+            <li><a href="">giải trí</a></li>
+        </ul>
+        <ul class="menu-submain">
+            <li><a href="">Thời sự</a></li>
+            <li><a href="">góc nhìn</a></li>
+            <li><a href="">thế giới</a></li>
+            <li><a href="">kinh doanh</a></li>
+            <li><a href="">giải trí</a></li>
+        </ul>
+    </div>
+    <hr>
+    <div class="footer-contact">
+        <div class="contact-logo">
+            <div class="logo">
+                <p>Báo điện tử</p>
+                <img src="assets/image/icon/logo-long.png" alt="">
+            </div>
+            <div class="detail">
+                <p>theo dõi TintucExpress trên</p>
                 <img src="assets/image/icon/facebook.svg" alt="">
                 <img src="assets/image/icon/youtube.svg" alt="">
+            </div>
+        </div>
+        
+        <hr>
+        <div class="contact-detail">
+            <div>
+                <h4>Báo tiếng việt có số lượng người xem nhiều</h6>
+                <p>Thuộc Team Larvel-Tintuc</p>
+            </div>
+            <div>
+                <p>Tổng biên tập: Laravel-Tintuc</p>
+                <p>Địa chỉ: Trường ĐH Sư phạm Kỹ Thuật</p>
+            </div>
+            <div>
+                <p>&copy;25/8-10/12/2022 TintucExpress dự trên bản quyền của VnEpress</p>
             </div>
         </div>
     </div>
