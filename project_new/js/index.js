d85c5ad14e4c705369f0c26a93e6af3f58c7fa41
chương lương <luongvanchuong963@gmail.com>
2022-12-11 11:22:33 +0700

    blog

--- project_new/js/index.js
+++ project_new/js/index.js
@@ -1,8 +1,33 @@
 const scrolll = document.querySelector(".hk");
+const scrchitiet = document.querySelector(".clct")
+const ic = document.querySelector(".ic")
 window.onscroll= function(){
-    if(window.scrollY >60){
+    console.log(window.scrollY)
+    if(window.scrollY >60 ){
         scrolll.classList.add("tivie")
+        if(window.scrollY>150){
+            ic.classList.add("iconi")
+        }
+        if(window.scrollY> 490){
+            scrchitiet.classList.add("sclctiet")
+            
+        }if(window.scrollY > 800){
+            ic.classList.remove("iconi")
+        }
+        if(window.scrollY < 490 || window.scrollY > 1000){
+            scrchitiet.classList.remove("sclctiet")
+        }
+        
     }else{
         scrolll.classList.remove("tivie")
+        // scrchitiet.classList.remove("sclctiet")
     }
 }
+
+// window.onscroll= function(){
+//     if(window.scrollY>568){
+        
+//     } else{
+        
+//     }
+// }
