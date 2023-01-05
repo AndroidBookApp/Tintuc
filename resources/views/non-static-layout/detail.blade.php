29f459a30259e0ac2035f6ab3c2f36d189ea8919
kiemluc01 <kiemluc01@gmail.com>
2023-01-05 22:15:23 +0700

    time

--- resources/views/non-static-layout/detail.blade.php
+++ resources/views/non-static-layout/detail.blade.php
@@ -26,7 +26,7 @@
                     @php  
                       $date = getdate();
                     @endphp
-                    <span>{{$date['wday']}}, {{$date['mday/mon/year']}}, {{$date['hours':'minutes']}} (GMT+7)</span>
+                    <span>{{$date['wday']}}, {{$date['mday/mon/year']}}, {{$date['hours:minutes']}} (GMT+7)</span>
                 </div>
                 <h2>{{$post['name']}}</h2>
                 <div class="nd">
