819432a51dc1401b2557fa4a37a1d663e0ef2ea1
kiemluc01 <kiemluc01@gmail.com>
2023-01-05 09:53:56 +0700

    link

--- resources/views/layout/main-layout.blade.php
+++ resources/views/layout/main-layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UTE NEWS</title>
-    <link rel="icon" href="{{ asset('assets/image/icon/logo.png') }}">
+    <link rel="icon" href="/assets/image/icon/logo.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
