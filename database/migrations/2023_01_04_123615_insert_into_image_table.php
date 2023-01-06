83d1bed85071d3218299613800236dcc3395e575
ThanhHung-488 <thanhhung14072001@gmail.com>
2023-01-06 18:06:31 +0700

    insert database

--- database/migrations/2023_01_04_123615_insert_into_image_table.php
+++ database/migrations/2023_01_04_123615_insert_into_image_table.php
@@ -21,6 +21,7 @@ class InsertIntoImageTable extends Migration
                 'post_id' => '1'
             )
         );
+
         DB::table('image')->insert(
             array(
                 'title' => 'Anh Thái Văn Tấn Tài (40 tuổi, cha Hạo Nam) ngóng chờ con ở hiện trường trong những ngày qua. Ảnh: Hoàng Nam',
@@ -28,6 +29,7 @@ class InsertIntoImageTable extends Migration
                 'post_id' => '1'
             )
         );
+
         DB::table('image')->insert(
             array(
                 'title' => 'Chị Nguyễn Thị Mỹ Linh trong căn nhà của mình ở xã Phú Lợi, huyện Thanh Bình, hôm 2/1, sau ba ngày con trai rơi xuống cọc bêtông ở công trường xây cầu. Ảnh: Ngọc Tài',
@@ -35,6 +37,49 @@ class InsertIntoImageTable extends Migration
                 'post_id' => '1'
             )
         );
+
+        /* id2 */
+        DB::table('image')->insert(
+            array(
+                'title' => 'Những cuốn sách mà con chị Mai không sử dụng. Ảnh: Thanh Hằng',
+                'link' => 'https://i1-vnexpress.vnecdn.net/2022/12/30/1be8247492ac4af213bd-1566-1672400771.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=jw3QTAblFiuZkjvFwOG7oQ',
+                'post_id' => '2'
+            )
+        );
+
+        DB::table('image')->insert(
+            array(
+                'title' => 'Một số sách năm nay mà con chị Liễu vẫn chưa sử dụng, dù học kỳ I đã trôi qua. Ảnh: Thanh Hằng',
+                'link' => 'https://i1-vnexpress.vnecdn.net/2022/12/30/322163132-1571180533295573-422-2098-8380-1672400771.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=ldhNVV1zHoeEyt670-1dNQ',
+                'post_id' => '2'
+            )
+        );
+
+        DB::table('image')->insert(
+            array(
+                'title' => 'Vốn hóa Amazon trong hơn một năm qua. Đồ thị: Bloomberg',
+                'link' => 'https://i1-kinhdoanh.vnecdn.net/2022/11/10/amazon-cap-png-1668066880-1668-5503-8173-1668067026.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=XdBd51MhT-_SAwNXGae9lw',
+                'post_id' => '3'
+            )
+        );
+
+        DB::table('image')->insert(
+            array(
+                'title' => 'Lấy lại tiền lì xì vì bị chê ít. Ảnh minh họa',
+                'link' => 'https://i1-giadinh.vnecdn.net/2022/12/28/307517829-506594918144087-6124-3100-9589-1672218963.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=RrbaC-T5G7N2ZpcRPnKXOA',
+                'post_id' => '4'
+            )
+        );
+
+        DB::table('image')->insert(
+            array(
+                'title' => 'Cục diện chiến sự Ukraine. Đồ họa: WP.',
+                'link' => 'https://i1-vnexpress.vnecdn.net/2023/01/06/556318717813726673a-Ukraine-Ng-3008-2981-1672970325.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=1u1JLIrCoYijxq2xY1WTWQ',
+                'post_id' => '5'
+            )
+        );
+
+        
     }
 
     /**
